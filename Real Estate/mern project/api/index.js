b2879b6242cc420c30fad97088080966284af33e
Anubhav-07 <139541501+Anubhav-07@users.noreply.github.com>
2025-06-30 10:32:12 +0530

    Update index.js

--- Real Estate/mern project/api/index.js
+++ Real Estate/mern project/api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 dotenv.config();
 
 const MONGO_URI = "mongodb+srv://User_1:7WNG8MrcnZUeRpXL@mongotube.fkafwkz.mongodb.net/estate?retryWrites=true&w=majority&appName=MongoTube"
@@ -15,6 +16,8 @@ mongoose.connect(MONGO_URI)
     .catch((err) => {
         console.error(err);
     });
+
+    const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -29,6 +32,13 @@ app.listen(PORT, () => {
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use('/api/listing',listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res) =>{
+    res.sendFile(path.join(__dirname,'client', 'dist', 'index.html'));
+})
+
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
     const message=err.message||'Internal Server Error';
